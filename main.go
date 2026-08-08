6c9101c19822442b1aac0af5a48726befda12f9f
Alex-T-27 <alextran201007@gmail.com>
2026-08-08 01:29:47 -0400

    Get a job posting and turn it into a readable file (HTML only)

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
-package main 
+package main
 
 import (
-	"fmt"
-	"net/http"
-	"encoding/json"
 	"bytes"
-	"os"
-	"github.com/joho/godotenv"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Data struct {
@@ -25,7 +27,7 @@ func main() {
 	token := os.Getenv("DISCORD_TOKEN")
 
 	// The content
-	body := Data{Content: "Hello World!!!"}
+	body := Data{Content: "I'm fetching TikTok's Job Posting LMAO JAJAJA"}
 
 	// Creates a new client object
 	client := &http.Client{}
@@ -35,26 +37,27 @@ func main() {
 		fmt.Println("Error:", err)
 		return
 	}
-	final_content := bytes.NewBuffer(jsonData) 
+	final_content := bytes.NewBuffer(jsonData)
 
 	// The URL to my discord server
-	url := fmt.Sprintf("https://discord.com/api/v10/channels/%s/messages", channelID)
+	discord_url := fmt.Sprintf("https://discord.com/api/v10/channels/%s/messages", channelID)
 
 	// Creates a request containing METHOD, URL, CONTENT
 	req, err := http.NewRequest(
 		"POST",
-		url,
+		discord_url,
 		final_content,
 	)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-	
+
 	// Modify the request's Headers
-	req.Header.Add("Authorization", "Bot " + token)
+	req.Header.Add("Authorization", "Bot "+token)
 	req.Header.Add("Content-Type", "application/json")
 
+	// Tell the Client to response to that request
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -63,4 +66,44 @@ func main() {
 	defer resp.Body.Close()
 
 	fmt.Println(resp.Status)
+
+	// Todo: Fetch one company's job posting and send it to discord
+
+	job_url := "https://lifeattiktok.com/search/7669712589169117445?spread=5MWH5CQ"
+	req, err = http.NewRequest(
+		"GET",
+		job_url,
+		nil,
+	)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	resp, err = client.Do(req)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println(resp.Status)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(resp.Header.Get("Content-Length"))
+
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		fmt.Println(len(bodyBytes))
+
+		err = os.WriteFile("tiktok.html", bodyBytes, 0644)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+	}
 }
