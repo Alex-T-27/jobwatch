b41a1909b52b4af8790e95ba678c3e38f1065504
Alex-T-27 <alextran201007@gmail.com>
2026-08-19 03:14:10 -0400

    Restructure code

--- main.go
+++ main.go
@@ -28,23 +28,11 @@ type JobList struct {
 	Jobs []Job `json:"jobs"`
 }
 
-func main() {
-	// Loads .env content
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
-	}
+// Creates a new client object
+var client = &http.Client{}
 
-	channelID := os.Getenv("CHANNEL_ID")
-	token := os.Getenv("DISCORD_TOKEN")
-
-	// Creates a new client object
-	client := &http.Client{}
-
-	// Todo: Fetch one company's job posting and turn it into readable file
-
+func fetchJobs(company string) ([]Job, error) {
 	job_list := JobList{}
-	company := "Deepgram"
 	job_url := fmt.Sprintf("https://api.ashbyhq.com/posting-api/job-board/%s", company)
 	req, err := http.NewRequest(
 		"GET",
@@ -53,76 +41,13 @@ func main() {
 	)
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
+		return nil, err
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	fmt.Println(resp.Status)
-	fmt.Println(resp.Header.Get("Content-Type"))
-	fmt.Println(resp.Header.Get("Content-Length"))
-
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-
-		err = json.Unmarshal(bodyBytes, &job_list)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-
-	}
-
-	//Todo: one Deepgram job appears in discord as readable message.
-	//Fetch -> Decode -> Pick jobs -> Format -> Send
-	//Checkpoint: Be able to print job[0] title to the terminal
-
-	job := job_list.Jobs[62]
-	//Message content
-	message := fmt.Sprintf("***New Job Found*** \n**%s - %s**\n%s\n%s", job.Title, company, job.Location, job.JobUrl)
-
-	// The content
-	body := Data{Content: message}
-
-	jsonData, err := json.Marshal(body) // Return a []byte @ jsonData
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	final_content := bytes.NewBuffer(jsonData)
-
-	// The URL to my discord server
-	discord_url := fmt.Sprintf("https://discord.com/api/v10/channels/%s/messages", channelID)
-
-	// Creates a request containing METHOD, URL, CONTENT
-	req, err = http.NewRequest(
-		"POST",
-		discord_url,
-		final_content,
-	)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	// Modify the request's Headers
-	req.Header.Add("Authorization", "Bot "+token)
-	req.Header.Add("Content-Type", "application/json")
-
-	// Tell the Client to response to that request
-	resp, err = client.Do(req)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		return nil, err
 	}
 	defer resp.Body.Close()
 
@@ -132,10 +57,124 @@ func main() {
 		bodyError, err := io.ReadAll(resp.Body)
 		if err != nil {
 			fmt.Println("Error:", err)
-			return
+			return nil, err
+		}
+		return nil, fmt.Errorf("ashby returned %s: %s", resp.Status, bodyError)
+	}
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil, err
+	}
+
+	err = json.Unmarshal(bodyBytes, &job_list)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil, err
+	}
+
+	return job_list.Jobs, nil
+}
+
+func formatJob(job Job, company string) string {
+	//Message content
+	message := fmt.Sprintf("***New Job Found*** \n**%s - %s**\n%s\n%s",
+		job.Title,
+		company,
+		job.Location,
+		job.JobUrl,
+	)
+	return message
+}
+
+func sendToDiscord(message string) error {
+	channelID := os.Getenv("CHANNEL_ID")
+	token := os.Getenv("DISCORD_TOKEN")
+
+	// The content
+	body := Data{Content: message}
+
+	jsonData, err := json.Marshal(body) // Return a []byte @ jsonData
+	if err != nil {
+		fmt.Println("Error:", err)
+		return err
+	}
+	final_content := bytes.NewBuffer(jsonData)
+
+	// The URL to my discord server
+	discord_url := fmt.Sprintf(
+		"https://discord.com/api/v10/channels/%s/messages",
+		channelID,
+	)
+
+	// Creates a request containing METHOD, URL, CONTENT
+	req, err := http.NewRequest(
+		"POST",
+		discord_url,
+		final_content,
+	)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return err
+	}
+
+	// Modify the request's Headers
+	req.Header.Add("Authorization", "Bot "+token)
+	req.Header.Add("Content-Type", "application/json")
+
+	// Tell the Client to response to that request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	fmt.Println(resp.Status)
+
+	if resp.StatusCode != http.StatusOK {
+		bodyError, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return err
 		}
 
-		fmt.Println(string(bodyError))
+		return fmt.Errorf("ashby returned %s: %s", resp.Status, bodyError)
 
 	}
+
+	return nil
+}
+
+func main() {
+	// Loads .env content
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Todo: Fetch one company's job posting and turn it into readable file
+
+	company := "Deepgram"
+
+	//Todo: one Deepgram job appears in discord as readable message.
+	//Fetch -> Decode -> Pick jobs -> Format -> Send
+	//Checkpoint: Be able to print job[0] title to the terminal
+
+	jobs, err := fetchJobs(company)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	job := jobs[60]
+	message := formatJob(job, company)
+
+	sendToDiscord(message)
+	// Todo: Break main() into three functions:
+	// fetchJobs(company string)  -> []Job, error
+	// formatJob(job Job)         -> string
+	// sendToDiscord(msg string)  -> error
+
+	// Todo: prints all job titles in terminal, filter jobs then send to Discord desired jobs
 }
